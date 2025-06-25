e3fb33fb9794d9eab543fbd1181a85824365c85c
BederCC <beder1810@gmail.com>
2025-06-24 23:31:47 -0500

    first commit

---
+++ 03_arreglos.php
@@ -0,0 +1,44 @@
+<?php
+// Definición de variables
+$course = "Curso de PHP";
+$archived = true;
+$status = $archived ? "archivado" : "activo";
+
+$tags = [
+    "PHP",
+    "Programación",
+    "Desarrollo Web"
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $course?></title>
+</head>
+<body>
+    <h1>
+        Bienvenido al <?= $course; ?>
+    </h1>
+
+    <p>
+        En este curso aprenderás desde lo más básico hasta lo más avanzado de PHP. 
+        Comenzaremos con la sintaxis básica, variables, tipos de datos y estructuras de control.
+    </p>
+
+    <p>
+        El estado del curso es: <?= $status; ?>.
+    </p>
+
+    <p>
+        Este curso está etiquetado con: 
+        <ul>
+            <li><?= $tags[0]; ?></li>
+            <li><?= $tags[1]; ?></li>
+            <li><?= $tags[2]; ?></li>
+        </ul>
+    </p>
+</body>
+</html>
